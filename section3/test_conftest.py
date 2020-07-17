d3a3df77fa32590563bb6f663b35c62350844c12
rrudiuk <rus.barik@gmail.com>
2020-07-17 20:04:09 +0300

    Language parameter task

--- section3/test_conftest.py
+++ section3/test_conftest.py
@@ -2,4 +2,4 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    add_to_card_btn = browser.find_element_by_css_selector("#login_link")
